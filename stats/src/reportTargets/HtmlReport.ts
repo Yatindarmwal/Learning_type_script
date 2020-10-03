c346bc7492b641dfc373deaf6483123daeb6e373
Yatin darmwal <yatin@infeedo.com>
2020-10-03 17:12:22 +0530

    stats complete

---
+++ stats/src/reportTargets/HtmlReport.ts
@@ -0,0 +1,13 @@
+import { OutputTarget } from '../Summery';
+import fs from 'fs';
+export class HtmlReport implements OutputTarget {
+    print(report: string): void {
+        const html = `
+        <div>
+        <h1>Analysis output</h1>
+        <div>${report}</div>
+        </div>
+        `;
+        fs.writeFileSync('report.html', html);
+    }
+}
